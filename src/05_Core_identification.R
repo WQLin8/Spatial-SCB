468d20e18f2e7208fb8ecfe6e411bbbc5ad2c04e
WQLin8 <144367859+WQLin8@users.noreply.github.com>
2026-05-28 11:43:21 -0500

    Update 05_Core_identification.R

--- src/05_Core_identification.R
+++ src/05_Core_identification.R
@@ -20,11 +20,11 @@ plot_Core(obj, "Osteoblast_Core", "osteoblast_Core.pdf")
 
 filter_all_samples <- function(obj) {
   sample_paths <- list(
-    "hd_68_1" = "Human_HD_68_batch1/outs/binned_outputs/square_056um/spatial/tissue_positions.parquet",
-    "hd_68_2" = "Human_HD_68_batch2/outs/binned_outputs/square_056um/spatial/tissue_positions.parquet",
-    "hd_82_1" = "Human_HD_82_batch1/outs/binned_outputs/square_056um/spatial/tissue_positions.parquet",
-    "hd_82_2" = "Human_HD_82_batch2/outs/binned_outputs/square_056um/spatial/tissue_positions.parquet",
-    "visium_12" = "Human_Visium_12/spatial/tissue_positions.csv"
+    "hd_03" = "Human_HD_03/outs/binned_outputs/square_056um/spatial/tissue_positions.parquet",
+    "hd_01" = "Human_HD_01/outs/binned_outputs/square_056um/spatial/tissue_positions.parquet",
+    "hd_04" = "Human_HD_04/outs/binned_outputs/square_056um/spatial/tissue_positions.parquet",
+    "hd_02" = "Human_HD_02/outs/binned_outputs/square_056um/spatial/tissue_positions.parquet",
+    "v_01" = "Human_V_01/spatial/tissue_positions.csv"
   )
   
   obj_list <- lapply(names(sample_paths), function(sid) {
